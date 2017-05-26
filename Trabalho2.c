0b290692b9c2e7ab46040bf1ab5ee6466d6e6198
Matheus Stauffer <matheus_stauffer@live.com>
2017-05-26 16:53:10 -0300

    Segundo Trabalho: Lista de Contatos

---
+++ Trabalho2.c
@@ -0,0 +1,232 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct inicio Inicio;
+typedef struct agenda Agenda;
+
+struct Inicio{
+	Agenda* prim;
+};
+
+struct Agenda{
+	char nome[30];
+	char email[40];
+	char telefone[10];
+	Agenda* prox;
+};
+
+Inicio* CriarLista(void){
+	Inicio* l = (Inicio*) malloc(sizeof(Inicio));
+	l->prim = NULL;
+	return l;
+}/*Cria a lista inicial*/
+
+void Inserir(Inicio* l){
+	Agenda* novo = (Agenda*) malloc(sizeof(Agenda));
+	printf("Insira o nome do contato: \n");
+	scanf("%[^\n]s%*c", novo->nome);
+	printf("Insira o endereço eletronico do contato: \n");
+	scanf("%[^\n]s%*c", novo->email);
+	printf("Insira o telefone do contato: \n");
+	scanf("%[^\n]s%*c", novo->telefone);
+	novo->prox = l->prim;
+	l->prim = novo;
+}
+
+void ImprimirCTT(Inicio* l){
+	Agenda* p;
+	int j;
+	char validador;
+	char nomectt[30];
+
+	printf("Informe o nome do contato que deseja visualizar:\n");
+	scanf("%[^\n]s%*c", nomectt);
+	j = 0;
+	for(p = l->prim; p!=NULL && j!= 1; p = p->prox){
+		if(p->nome == nomectt){
+			printf("\t\tCONTATO\t\t\n");
+			printf("\t\t%s\t\t\n", p->nome);
+			printf("\t\t%s\t\t\n", p->email);
+			printf("\t\t%s\t\t\n", p->telefone);
+		}
+		j = 1;
+	}
+	if(j==0){
+		printf("O contato buscado nao existe. Deseja inclui-lo? (S/N)\n");
+		scanf("%c", &validador);
+		if(validador == 'S' || validador == 's'){
+			Inserir(l);
+		}
+	}
+}
+
+void ImprimirTudo(Inicio* l){
+	Agenda* p;
+
+	for(p = l->prim; p != NULL; p = p->prox){
+		printf("nome: \n", p->nome);
+		printf("email: \n", p->email);
+		printf("telefone: \n", p->telefone);
+	}	
+}
+
+void Ordenar(Inicio* l){
+	Agenda* p;
+	Agenda* aux;
+	int qual;
+	char s[30];
+
+	printf("Como deseja ordenar seus contatos? Se quiser ordenar por nome, digite 1.\n");
+	printf("Caso queira ordenar por email, digite 2, e se quiser por telefone, 3.\n");
+	scanf("%d", &qual);
+	/*Os contatos sao ordenados pelo algortimo BubbleSort, de acordo com a escolha do
+	usuario: por nome, email ou telefone. Sao utilizadas funçoes da biblioteca
+	string.h para tornar o codigo menos denso.*/
+	if(qual == 1){
+		while(aux!=NULL){
+			p = aux->prox;
+			while(p!=NULL){
+				if(strcmp(aux->nome, p->nome) > 0){
+					strcpy(s, aux->nome);
+					strcpy(aux->nome, p->nome);
+					strcpy(p->nome, s);
+				}
+				p = p->prox;
+			}
+			aux = aux->prox;
+		}
+	}/*Fim ordenaçao por NOME*/
+	else{
+		if(qual == 2){
+			while(aux!=NULL){
+				p = aux->prox;
+				while(p!=NULL){
+					if(strcmp(aux->email, p->email) > 0){
+						strcpy(s, aux->email);
+						strcpy(aux->email, p->email);
+						strcpy(p->email, s);
+					}
+					p = p->prox;
+				}
+				aux = aux->prox;
+			}
+		}/*Fim ordenaçao por EMAIL*/
+		else{
+			while(aux!=NULL){
+				p = aux->prox;
+				while(p!=NULL){
+					if(strcmp(aux->telefone, p->telefone) > 0){
+						strcpy(s, aux->telefone);
+						strcpy(aux->telefone, p->telefone);
+						strcpy(p->telefone, s);
+					}
+					p = p->prox;
+				}
+				aux = aux->prox;
+			}
+		}		
+	}/*Fim Bloco Else (qual != 1)*/
+}
+
+void Editar(Inicio* l){
+	Agenda* p;
+	char s[30], nvnome[30], nvemail[40], nvtelefone[10];
+	char keep;
+	int j;
+
+	printf("Digite o nome do contato que deseja editar: \n");
+	scanf("%[^\n]s%*c", s);
+	j = 1;
+	for(p = l->prim; p!=NULL && j!=0; p = p->prox){
+		if(s == p->nome){
+			printf("Digite o novo nome do contato:\n");
+			scanf("%[^\n]s%*c", nvnome);
+			p->nome = nvnome;
+			printf("Digite o novo email do contato:\n");
+			scanf("%[^\n]s%*c", nvemail);
+			p->email = nvemail;
+			printf("Digite o novo telefone do contato:\n");
+			scanf("%[^\n]s%*c", nvtelefone);
+			p->telefone = nvtelefone;
+			j = 0;
+		}
+	}
+	printf("Deseja editar mais algum contato?(S/N)\n");
+	scanf("%c", &keep);
+	if(keep == 'S' || keep == 's'){
+		Editar(l);
+	}
+}
+
+void Remover(Inicio* l){
+	Agenda* ant = NULL;
+	Agenda* p = l->prim;
+	char keep;
+	char nomectt[30];
+
+	printf("Informe o nome do contato que deseja remover:\n");
+	scanf("%[^\n]s%*c", nomectt);
+	while(p != NULL && p->nome != nomectt){
+		ant = p;
+		p = p->prox;
+	}
+	if(p != NULL){
+		if(ant == NULL){
+			l->prim = p->prox;
+			/*O elemento a ser removido foi encontrado na primeira posiçao da lista,
+			cujo ponteiro passa a apontar para o proximo elemento, afim de que o
+			no´ encontrado possa ser removido.*/
+		}
+		else{
+			ant->prox = p->prox;
+			/*O elemento a ser removido foi encontrado no meio lista. Para efetuar a
+			operaçao, precisamos que a referencia(Ponteiro) do elemento anterior passe
+			a ser a do elemento removido, a fim de manter o encadeamento da lista.*/
+		}
+		free(p);
+	}
+
+	printf("Deseja remover mais algum elemento?(S/N)\n");
+	scanf("%c", &keep);
+	if(keep == 'S' || keep == 's'){
+		Editar(l);
+	}
+}
+
+int main(){
+	int keep;
+
+	Inicio* l = CriarLista();
+	do{
+		printf("\t\t\tAGENDA DE CONTATOS\t\t\t\n");
+		printf("Informe a operaçao que deseja realizar:\n");
+		printf("1 - Inserir novo contato, 2 - Ordenar Lista de Contatos,\n");
+		printf("3 - Imprimir toda Lista de Contatos, 4 - Imprimir Contato\n");
+		printf("especifico, 5 - Editar um contato e 6 - Remover contato.\n");
+		printf("Caso deseja terminar a execuçao, digite 7.\n");
+		scanf("%d", &keep);
+		switch(keep){
+			case 1:
+				Inserir(l);
+				break;
+			case 2:
+				Ordenar(l);
+				break;
+			case 3:
+				ImprimirTudo(l);
+				break;
+			case 4:
+				ImprimirCTT(l);
+				break;
+			case 5:
+				Editar(l);
+				break;
+			case 6:
+				Remover(l);
+		}
+	}while(keep != 7);
+
+
+	return 0;
+}
